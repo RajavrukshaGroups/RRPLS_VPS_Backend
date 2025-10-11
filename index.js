6d12fe7492d6b488f080b10a67867f67eb478456
root <root@6.88.74.97.host.secureserver.net>
2025-10-11 10:21:46 +0000

    committing conflict

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const { dbConnect } = require('./config/config.js');
 const userRoute = require('./routes/routes.js');
 const app = express();  
-const port = 3000;
+const port = 5000;
 
 // Connect to the database
 dbConnect();
